c2cbc8be6fc36c2b94ac79e1259f5cfb83e7504c
RajSM139 <cho2mishra@gmail.com>
2024-09-18 20:24:28 +0530

    chore: added comments

--- functions/start-talk.protected.js
+++ functions/start-talk.protected.js
@@ -7,16 +7,27 @@ http://localhost:3000/start-talk?TalkCode=demo
 */
 
 exports.handler = async (context, event, callback) => {
+  /* This block of code is performing the following tasks: */
   const data = new Data(context);
   const client = context.getTwilioClient();
   const code = event.TalkCode;
   const talk = data.getTalkByCode(code);
   const speakerCallSids = [];
   console.log("Calling speakers...");
+  /* The code block `let domainName = context.DOMAIN_NAME;
+  if (domainName.startsWith("localhost")) {
+    domainName = "<YOUR NGROK URL>";
+  }` is checking if the `domainName` extracted from `context.DOMAIN_NAME` starts with "localhost".
+  If it does, it means that the code is running locally, and in that case, it sets the `domainName`
+  variable to "<YOUR NGROK URL>". This is typically done to handle local development environments
+  where ngrok is used to expose a local server to the internet for testing webhook callbacks or
+  external API integrations. */
   let domainName = context.DOMAIN_NAME;
   if (domainName.startsWith("localhost")) {
     domainName = "<YOUR NGROK URL>";
   }
+  /* This block of code is iterating over each speaker in the `talk.speakers` array and initiating a
+  call to each speaker using Twilio's `conferences` API. */
   for (const speaker of talk.speakers) {
     const participant = await client
       .conferences(talk.code)
@@ -33,12 +44,15 @@ exports.handler = async (context, event, callback) => {
     speakerCallSids.push(participant.callSid);
   }
 
+  /* This block of code is performing the following tasks: */
   const registrants = await data.getRegistrants(talk);
   console.log("Calling registrants....");
   let registrantPhoneNumbers = registrants.map((r) => r.phoneNumber);
   const registrantsPhoneNumbersSet = new Set(registrantPhoneNumbers);
   registrantPhoneNumbers = [...registrantsPhoneNumbersSet];
 
+  /* The `const promises = registrantPhoneNumbers.map(async (registrantPhoneNumber) => { ... }` code
+  block is creating an array of promises. */
   const promises = registrantPhoneNumbers.map(async (registrantPhoneNumber) => {
     const participant = await client
       .conferences(talk.code)
@@ -52,6 +66,9 @@ exports.handler = async (context, event, callback) => {
     return participant.callSid;
   });
 
+  /* This block of code is handling the asynchronous processing of calling registrants by creating an
+  array of promises using `registrantPhoneNumbers.map`. Each promise represents the process of
+  creating a participant in a conference for a registrant phone number. */
   const results = await Promise.allSettled(promises);
   const registrantCallSids = results
     .filter((result) => result.status === "fulfilled")
@@ -68,6 +85,8 @@ exports.handler = async (context, event, callback) => {
           registrantPhoneNumbers.length - registrantCallSids.length
         }
     `);
+  /* The `callback(null, { talkCode: event.TalkCode, speakerCallSids, registrantCallSids });` statement
+  is used to send a response back to the caller of this function. */
   callback(null, {
     talkCode: event.TalkCode,
     speakerCallSids,
